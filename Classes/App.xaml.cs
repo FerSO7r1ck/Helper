a36791907ff7060df23f3d30ec68bbae452866ce
FerSO7 <87352038+FerSO7r1ck@users.noreply.github.com>
2022-08-01 13:53:51 -0300

    Classes

---
+++ Classes/App.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Runtime.InteropServices;
+using UnityPlayer;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPApp
+{
+    /// <summary>
+    /// Proporciona un comportamiento específico de la aplicación para complementar la clase Application predeterminada.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        private AppCallbacks m_AppCallbacks;
+        public SplashScreen splashScreen;
+
+        // In C++ below would be:
+        // __declspec(dllimport) void __stdcall AddActivatedEventArgs(IInspectable* activatedEventArgs)
+       //  [DllImport("GameAssembly.dll")]
+        // static extern void AddActivatedEventArgs(IActivatedEventArgs args);
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+           // CoreApplication.GetCurrentView().Activated += OnAppActivated;
+
+            this.InitializeComponent();
+            SetupOrientation();
+            m_AppCallbacks = new AppCallbacks();
+        }
+
+        /// <summary>
+        /// Invoked when application is launched through protocol.
+        /// Read more - http://msdn.microsoft.com/library/windows/apps/br224742
+        /// </summary>
+        /// <param name="args"></param>
+       /* protected override void OnActivated(IActivatedEventArgs args)
+        {
+            string appArgs = "";
+
+            switch (args.Kind)
+            {
+                case ActivationKind.Protocol:
+                    ProtocolActivatedEventArgs eventArgs = args as ProtocolActivatedEventArgs;
+                    splashScreen = eventArgs.SplashScreen;
+                    appArgs += string.Format("Uri={0}", eventArgs.Uri.AbsoluteUri);
+                    break;
+            }
+            InitializeUnity(appArgs);
+        }*/
+
+        /// <summary>
+        /// Invoked when application is launched via file
+        /// Read more - http://msdn.microsoft.com/library/windows/apps/br224742
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnFileActivated(FileActivatedEventArgs args)
+        {
+            string appArgs = "";
+
+            splashScreen = args.SplashScreen;
+            appArgs += "File=";
+            bool firstFileAdded = false;
+            foreach (var file in args.Files)
+            {
+                if (firstFileAdded) appArgs += ";";
+                appArgs += file.Path;
+                firstFileAdded = true;
+            }
+
+            InitializeUnity(appArgs);
+        }
+
+        /// <summary>
+        /// Se invoca cuando la aplicación la inicia normalmente el usuario final. Se usarán otros puntos
+        /// de entrada cuando la aplicación se inicie para abrir un archivo específico, por ejemplo.
+        /// </summary>
+        /// <param name="e">Información detallada acerca de la solicitud y el proceso de inicio.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            splashScreen = args.SplashScreen;
+            InitializeUnity(args.Arguments);
+        }
+
+        /// <summary>
+        /// Se invoca cuando la aplicación la inicia normalmente el usuario final. Se usarán otros puntos
+        /// </summary>
+        /// <param name="sender">Marco que produjo el error de navegación</param>
+        /// <param name="e">Detalles sobre el error de navegación</param>
+        private void InitializeUnity(string args)
+        {
+           ApplicationView.GetForCurrentView().SuppressSystemOverlays = true;
+
+            m_AppCallbacks.SetAppArguments(args);
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null && !m_AppCallbacks.IsInitialized())
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+#if !UNITY_HOLOGRAPHIC
+                Window.Current.Activate();
+#endif
+                rootFrame.Navigate(typeof(MainPage));
+            }
+
+            Window.Current.Activate();
+        }
+
+        void SetupOrientation()
+        {
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape | DisplayOrientations.LandscapeFlipped | DisplayOrientations.Portrait | DisplayOrientations.PortraitFlipped;
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+        }
+    }
+}
